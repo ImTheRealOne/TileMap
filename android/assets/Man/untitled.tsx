d807f2c6f3e36be8ca431de0c233bb5e3ac28ceb
MinU <kowasockilendaro@yahoo.com>
2017-10-28 17:30:31 -0700

    Update : First push for TiledMap Needs : Fix ortho camera angle

---
+++ android/assets/Man/untitled.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Man" tilewidth="256" tileheight="512" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="512" source="man-ne.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="512" source="man-nw.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="512" source="man-sw.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="512" source="man-se.png"/>
+ </tile>
+</tileset>
---
+++ android/assets/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="preview" tilewidth="32" tileheight="32" tilecount="209" columns="19">
+ <image source="preview.png" width="620" height="376"/>
+</tileset>
